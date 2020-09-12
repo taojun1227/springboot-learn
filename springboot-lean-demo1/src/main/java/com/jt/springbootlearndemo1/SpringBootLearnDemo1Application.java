f2029f9af56db548cc05dd4d1c2c0b79d52a5788
taojun1227 <470804486@qq.com>
2020-09-12 13:24:49 +0800

    springboot 项目初创建

---
+++ springboot-lean-demo1/src/main/java/com/jt/springbootlearndemo1/SpringBootLearnDemo1Application.java
@@ -0,0 +1,23 @@
+package com.jt.springbootlearndemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Title: SpringBootLearnDemo1Application </p>
+ *
+ * <p>Description:  启动类</p>
+ *
+ * <p>Company: </p>
+ *
+ * <p>date: 2020/9/12 12:46 </p>
+ *
+ * @author: jt-ape
+ */
+@SpringBootApplication
+public class SpringBootLearnDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearnDemo1Application.class, args);
+    }
+}
