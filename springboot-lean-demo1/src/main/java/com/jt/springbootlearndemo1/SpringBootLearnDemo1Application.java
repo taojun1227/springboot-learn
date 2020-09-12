12d4266719a948cf83c956a9d92afacb0d007ebf
taojun1227 <470804486@qq.com>
2020-09-12 17:23:22 +0800

    SpringBoot jar包部署demo

--- springboot-lean-demo1/src/main/java/com/jt/springbootlearndemo1/SpringBootLearnDemo1Application.java
+++ springboot-lean-demo1/src/main/java/com/jt/springbootlearndemo1/SpringBootLearnDemo1Application.java
@@ -2,6 +2,8 @@ package com.jt.springbootlearndemo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>Title: SpringBootLearnDemo1Application </p>
@@ -15,9 +17,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author: jt-ape
  */
 @SpringBootApplication
+@RestController
 public class SpringBootLearnDemo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootLearnDemo1Application.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+
+        return "hello springboot";
     }
 }
